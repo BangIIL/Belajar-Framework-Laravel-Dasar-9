99aba69e08d5e077f33875177eabad61c0458b24
Syaprilla Donata <syaprilladonata@gmail.com>
2024-03-26 08:53:55 +0700

    Memasukan Data Registrasi ke Database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
+use Hash;
 use Session;
-
-
 
 class AuthController extends Controller
 {
@@ -47,5 +47,13 @@ class AuthController extends Controller
             'email' => 'required|email|unique:users',
             'password' => 'required|min:6|confirmed',
         ]);
+
+        User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return redirect('login');
     }
 }
